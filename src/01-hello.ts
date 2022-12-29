b38b9fa44cec0e148651a00c8ba171192b944737
EugeBr <eugebordenave@gmail.com>
2022-12-29 12:15:11 +0100

    sample compilation

---
+++ src/01-hello.ts
@@ -0,0 +1,2 @@
+const myName = 'Eugenia';
+console.log(myName);
--- src/demo.ts
+++ src/demo.ts
@@ -35,6 +35,6 @@
       name: 'Nicolas',
       lastName: 'Molina'
     }
-    const rta = `${person} - ${limit}`;
-    console.log(rta);
+    //const rta = person + limit;
+    //console.log(rta);
   });
