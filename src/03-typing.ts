260d8710754820c00025f3e276ea69e152989bea
EugeBr <eugebordenave@gmail.com>
2022-12-29 13:06:06 +0100

    typing examples

---
+++ src/03-typing.ts
@@ -0,0 +1,15 @@
+(() => {
+let myProductName = 'Product 1';
+let myProductPrice = 123;
+
+myProductName = 'change';
+myProductName.toLocaleLowerCase();
+
+myProductPrice.toFixed();
+
+const myProductStock = 1000;
+const myProductname2 = 'Product 1';
+})();
+//funcion anonima autoejecutable
+//la usamos para utilizar las mismas variables en diferentes archivos sin que de error.
+//hace qye se encuentran en diferentes scopes.
