b248009c4d7f5193c4ad2aea6fdc8e36df0711c6
EugeBr <eugebordenave@gmail.com>
2023-01-04 11:39:21 +0100

    typing examples alias

---
+++ src/10-alias.ts
@@ -0,0 +1,21 @@
+(() =>{
+    // Crear un tipo
+    type UserID = string | number; //* type es una palabra reservada en typeScript
+    let userId: UserID;
+
+    // Literal types
+    type Sizes = 'S' | 'M' | 'L' | 'XL'; //*limita las opciones permitidas
+    let shirtSize: Sizes;
+    shirtSize = 'M';
+    shirtSize = 'L';
+    shirtSize = 'XL';
+    //shirtSize = 'shdfgsjkfg'; //incorrecto
+
+    function greeting(userId: UserID, size: Sizes) {
+        if (typeof userId === "string") {
+            console.log(`string ${userId.toLowerCase()}`);
+        }
+    };
+
+    greeting(1111, 'M');
+})();
